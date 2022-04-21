37580ec81f21fee37e87df71bf13948a0cf12472
Michał Olesiak <molesiak099@gmail.com>
2022-04-21 21:45:44 +0200

    do all exercieses from chapter 2, and add .gitignore

--- chapter02_Ex.js
+++ chapter02_Ex.js
@@ -1,1 +1,44 @@
-/* exercises01 */
+/* exercise01 */
+const printTriangle = function (size) {
+  let row = '';
+  for (let i = 0; i < size; i++) {
+    row += '#';
+    console.log(row);
+  }
+};
+// printTriangle(7);
+
+/* exercise02 */
+const printFizzBuzz = function () {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+};
+// printFizzBuzz();
+
+/* exercise03 */
+const printChessBoard = function (size) {
+  let row1 = '';
+  let row2 = '';
+  for (let i = 0; i < size / 2; i++) {
+    for (let i = 0; i < size / 2; i++) {
+      row1 = row1 + '#' + '   ';
+      row2 = row2 + '   ' + '#';
+    }
+    row1 += '\n';
+    row2 += '\n';
+    console.log(row1);
+    console.log(row2);
+    row1 = '';
+    row2 = '';
+  }
+};
+// printChessBoard(8);
